ad2da28183fb393af105d4aae4d8434147f76176
leochiubowser <leochiubowser@gmail.com>
2022-08-21 17:05:44 +0800

    upudateScreen

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
+
 function updateScreen() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
