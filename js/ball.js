2fc133171490c44c2e042c0d86cce544f303bf1e
leochiubowser <leochiubowser@gmail.com>
2022-08-21 17:01:59 +0800

    ball physics

--- js/ball.js
+++ js/ball.js
@@ -1,10 +1,30 @@
 class Ball {
     constructor() {
-        this.x = canvas.width / 2;
-        this.y = canvas.height - 100;
-        this.radius = 15;
+        this.radius = 10;
+        this.x = window.innerWidth / 2;
+        this.y = window.innerHeight / 2;
+        this.maxSpeed = 3.5;
+        this.minSpeed = 2;
+        this.velocity = {
+            x: Math.round(Math.random() * (this.maxSpeed - this.minSpeed) + this.minSpeed),
+            y: Math.round(Math.random() * (this.maxSpeed - this.minSpeed) + this.minSpeed)
+        }
     }
+
+    UpdatePos() {
+        this.x += this.velocity.x;
+        this.y += this.velocity.y;
+
+        if (this.x + this.radius >= canvas.width || this.x - this.radius <= 0) {
+            this.velocity.x = - this.velocity.x;
+        } else if (this.y + this.radius >= canvas.height || this.y - this.radius <= 0) {
+            this.velocity.y = -this.velocity.y;
+        }
+    }
+
     draw() {
+        this.UpdatePos();
+
         ctx.save();
         ctx.fillStyle = "white";
         ctx.beginPath();
@@ -12,7 +32,6 @@ class Ball {
         ctx.closePath();
         ctx.fill();
         ctx.restore();
-        this.y -= 10;
     }
 
 }
--- js/index.js
+++ js/index.js
@@ -1,17 +1,19 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+function updateScreen() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
 
-
-var ball = new Ball();
+const ball = new Ball();
 
 animation();
 
-
 function animation() {
     requestAnimationFrame(animation);
+    updateScreen();
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    
+
     ball.draw();
 }
+
