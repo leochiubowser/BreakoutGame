e6518fc12917129ff1b28753c17cf9060bf49478
leochiubowser <leochiubowser@gmail.com>
2022-08-21 21:53:01 +0800

    upload

---
+++ js/enemy.js
@@ -0,0 +1,17 @@
+class Enemy {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.width = 80;
+        this.height = 10;
+        this.colors = ["red", "yellow"];
+        this.color = Math.round(Math.random() * 1);
+    }
+
+    draw() {
+        ctx.save();
+        ctx.fillStyle = this.colors[this.color];
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.restore();
+    }
+}
--- js/index.js
+++ js/index.js
@@ -5,14 +5,18 @@ canvas.height = window.innerHeight;
 
 const ball = new Ball();
 const player = new Player();
+const enemy = new Enemy(100, 100);
+
 
 animation();
 
+// (canvas.width - ((real + enemy.width) * enemyCount.Horizontal) + real) / 2
 function animation() {
     requestAnimationFrame(animation);
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     player.draw();
+    enemy.draw();
     ball.draw();
 }
 
